53e958744f3d64b04a6562a91150823ddedb2c24
franciscochoi <fchoi@itba.edu.ar>
2019-10-05 17:29:04 -0300

    Merge pull request #1 from gbarbieripederiva/startPage

--- src/components/dispositivosEnCuarto/dispositivos/horno.vue
+++ src/components/dispositivosEnCuarto/dispositivos/horno.vue
@@ -1,9 +1,28 @@
 <template>
-  <v-card fillWidth   color="white">horno</v-card>
+  <v-container fill-width>
+    <v-row>
+      <v-col>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-    name:"Horno"
+    name:"Horno",
+  data(){
+    return{
+      isOn: false,
+      temperature: 90,
+      heatSources:["convencional","abajo","arriba"],
+      heatSource:"convencional",
+      grillModes:["apagado", "económico", "completo"],
+      grillMode: "apagado",
+      convectionModes :["apagado", "económico", "completo"],
+      convectionMode : "apagado"
+
+    };
+  }
+
 }
 </script>
--- src/components/dispositivosEnCuarto/dispositivos/persiana.vue
+++ src/components/dispositivosEnCuarto/dispositivos/persiana.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container>
+  <v-container fill-width>
     <v-row>
       <v-col >
         <div align="center">
