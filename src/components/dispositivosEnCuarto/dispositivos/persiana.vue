35516cbeb32909b96dd809f706ef4ac955d2946d
franciscochoi <fchoi@itba.edu.ar>
2019-10-05 16:52:35 -0300

    Merge pull request #1 from gbarbieripederiva/startPage

--- src/components/dispositivosEnCuarto/dispositivos/persiana.vue
+++ src/components/dispositivosEnCuarto/dispositivos/persiana.vue
@@ -1,9 +1,40 @@
 <template>
-  <v-card fillWidth   color="white">persiana</v-card>
+  <v-container>
+    <v-row>
+      <v-col >
+        <div align="center">
+          <v-btn class="ma-2 lime lighten-2 black--text mx-3 ma-2" x-large @click="goUp">
+            <v-icon left>mdi-arrow-up-bold</v-icon>
+            <span>SUBIR</span>
+          </v-btn>
+          <v-btn class="ma-2 lime lighten-2 black--text mx-3 ma-2" x-large @click="goDown">
+            <v-icon left>mdi-arrow-down-bold</v-icon>
+            <span>BAJAR</span>
+          </v-btn>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-    name:"Persiana"
-}
+  name: "Persiana",
+  data() {
+    return {
+      isUp: true,
+    };
+  },
+  methods:{
+    goUp(){
+      this.isUp = true;
+    },
+    goDown(){
+      this.isUp = false;
+    }
+  }
+};
+
 </script>
+
+
