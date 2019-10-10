92d750806e4c29b1d712d7b3a299e8277779e9e9
franciscochoi <fchoi@itba.edu.ar>
2019-10-10 16:11:47 -0300

    Merge branch 'dev' into devBranchChoi
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- src/pages/loggedOffHome.vue
+++ src/pages/loggedOffHome.vue
@@ -7,15 +7,24 @@
       <v-row align="center" justify="center">
         <p class="display-3 yellow--text">Barsa Home</p>
       </v-row>
-      <v-row>
-        <v-col>
-          <p class="title">Misma casa, otra sensación</p>
+      <v-row class="my-8">
+        <v-col cols="4 mx-3">
+          <p class="title" justify="center" align="center">Misma casa, otra sensación</p>
           <p>
             Descubre una nueva forma de controlar
             su casa. Todo desde el alcance de su mano.
             Automatice el prendido y apagado de dispositivos
-            con nuestra aplicacion web
+            con nuestra aplicacion web. Acceda a cada lugar de su casa con solo un click y sin tener que mover un dedo. Desplazarse en su hogar
+            ya no es una obligación sino una descisión. Es facil, es simple, es Barsa.
           </p>
+        </v-col>
+        <v-spacer ></v-spacer>
+        <v-col cols="6">
+          <v-container >
+            <v-row align="center" justify="center">
+              <v-img :src="require('@/assets/smartHouse.jpg')" width="400"></v-img>
+            </v-row>
+          </v-container>
         </v-col>
         <v-col></v-col>
       </v-row>
@@ -25,6 +34,9 @@
 
 <script>
 export default {
-  name: "LoggedOffHome"
+  name: "LoggedOffHome",
+
+
+
 };
 </script>
